6c50e6f3fae74acef41e5d0ea1b45ef56a195f3d
Gus Rodriguez <gus@gustheitguru.com>
2017-03-06 11:20:10 -0700

    PUT API

--- server.js
+++ server.js
@@ -134,14 +134,14 @@ app.post('/todo', function(req, res) {
 });
 
 //Delete /todo/:id
-app.delete('/todo/:id', function (req, res) {
+app.delete('/todo/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
 	db.todo.destroy({
 		where: {
 			id: todoId
 		}
-	}).then(function (rowsDeleted) {
+	}).then(function(rowsDeleted) {
 		if (rowsDeleted === 0) {
 			res.status(404).json({
 				error: 'no to do with that ID'
@@ -171,30 +171,54 @@ app.delete('/todo/:id', function (req, res) {
 //PUT /todo/:id
 app.put('/todo/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
 	var body = _.pick(req.body, 'description', 'completed');
-	var validAttributes = {};
+	var attributes = {};
 
-	if (!matchedTodo) {
-		return res.status(404).send();
+	if (body.hasOwnProperty('completed')) {
+		attributes.completed = body.completed;
 	}
 
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send();
+	if (body.hasOwnProperty('description')) {
+		attributes.description = body.description;
 	}
 
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send();
-	}
+	db.todo.findById(todoId).then(function(todo) {
+		if (todo) {
+			return todo.update(attributes);
+		} else {
+			res.status(404).send();
+		}
+	}, function (){
+		res.status(500).send();
+	}).then(function (todo) {
+		res.json(todo.toJSON());
+	}, function(e) {
+		res.status(400).send(e);
+	});
 
-	_.extend(matchedTodo, validAttributes);
-	res.json(matchedTodo);
+
+	// var matchedTodo = _.findWhere(todos, {
+	// 	id: todoId
+	// });
+
+	// if (!matchedTodo) {
+	// 	return res.status(404).send();
+	// }
+
+	// if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+	// 	validAttributes.completed = body.completed;
+	// } else if (body.hasOwnProperty('completed')) {
+	// 	return res.status(400).send();
+	// }
+
+	// if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+	// 	validAttributes.description = body.description;
+	// } else if (body.hasOwnProperty('description')) {
+	// 	return res.status(400).send();
+	// // }
+	// _.extend(matchedTodo, validAttributes);
+	// res.json(matchedTodo);
+
 });
 
 db.sequelize.sync().then(function() {
